9b3af169dc9820d1d548241f1b3946eba5b8cbaf
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2023-06-24 16:54:23 -0300

    configurando projeto e suas bibliotecas

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export function Header() {
+    return (
+        <Head>
+            <title>Lista de Jogos</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content="Lista de Jogos - Processo Seletivo" />
+        </Head>
+    )
+}
---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import { HStack } from "@chakra-ui/react";
+
+export function Navbar() {
+    return (
+        <HStack
+            as="nav"
+        >
+            
+        </HStack>
+    )
+}
